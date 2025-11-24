9556a500ed9d7f0b45679833aecf0ac63ce5ee97
heng30 <2238288979@qq.com>
2025-11-24 10:55:30 +0800

    [*] use openh264 as video encoder for windows

--- lib/recorder/src/video_encoder.rs
+++ lib/recorder/src/video_encoder.rs
@@ -1,17 +1,11 @@
 #[cfg(feature = "x264-video-encoder")]
 mod ve_x264;
 
-use derive_setters::Setters;
-#[cfg(feature = "x264-video-encoder")]
-pub use ve_x264::*;
-
 #[cfg(feature = "openh264-video-encoder")]
 mod ve_openh264;
 
-#[cfg(feature = "openh264-video-encoder")]
-pub use ve_openh264::*;
-
 use crate::{FPS, RecorderError, recorder::ResizedImageBuffer};
+use derive_setters::Setters;
 
 #[derive(Debug, Clone)]
 pub enum EncodedFrame {
@@ -54,48 +48,10 @@ impl VideoEncoderConfig {
 
 pub fn new(config: VideoEncoderConfig) -> Result<Box<dyn VideoEncoder>, RecorderError> {
     #[cfg(feature = "x264-video-encoder")]
-    let ve = X264VideoEncoder::new(config)?;
+    let ve = ve_x264::X264VideoEncoder::new(config)?;
+
+    #[cfg(feature = "openh264-video-encoder")]
+    let ve = ve_openh264::OpenH264VideoEncoder::new(config)?;
 
     Ok(Box::new(ve))
-}
-
-pub fn rgb_to_i420_yuv(rgb_data: &[u8], width: u32, height: u32) -> Result<Vec<u8>, RecorderError> {
-    use yuv::{
-        YuvChromaSubsampling, YuvConversionMode, YuvPlanarImageMut, YuvRange, YuvStandardMatrix,
-        rgb_to_yuv420,
-    };
-
-    let frame_size = (width * height) as usize;
-
-    // Allocate YUV planar image
-    let mut planar_image =
-        YuvPlanarImageMut::<u8>::alloc(width, height, YuvChromaSubsampling::Yuv420);
-
-    // Convert RGB to YUV420
-    rgb_to_yuv420(
-        &mut planar_image,
-        rgb_data,
-        width * 3, // RGB stride (3 bytes per pixel)
-        YuvRange::Limited,
-        YuvStandardMatrix::Bt601,
-        YuvConversionMode::Balanced,
-    )
-    .map_err(|e| {
-        RecorderError::ImageProcessingFailed(format!("RGB to YUV conversion failed: {:?}", e))
-    })?;
-
-    // Extract the YUV data from the planar image
-    let mut yuv_data = vec![0u8; frame_size * 3 / 2];
-
-    // Copy Y plane
-    yuv_data[0..frame_size].copy_from_slice(planar_image.y_plane.borrow());
-
-    // Copy U plane
-    let u_plane_end = frame_size + frame_size / 4;
-    yuv_data[frame_size..u_plane_end].copy_from_slice(planar_image.u_plane.borrow());
-
-    // Copy V plane
-    yuv_data[u_plane_end..].copy_from_slice(planar_image.v_plane.borrow());
-
-    Ok(yuv_data)
 }
